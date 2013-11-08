0e8bd6d80cfc19bb8ec6b68934d9ddb61438329e
Tony Huynh <th1142@nyu.edu>
2013-11-08 00:54:34 -0500

    Platform collision on all sides. Try and break it! I haven't full tested it

--- noRestForTheQuery/noRestForTheQuery/noRestForTheQuery/Game1.cs
+++ noRestForTheQuery/noRestForTheQuery/noRestForTheQuery/Game1.cs
@@ -11,9 +11,6 @@ using Microsoft.Xna.Framework.Media;
 
 namespace noRestForTheQuery
 {
-    /// <summary>
-    /// This is the main type for your game
-    /// </summary>
     public class Game1 : Microsoft.Xna.Framework.Game
     {
         GraphicsDeviceManager graphics;
@@ -22,9 +19,14 @@ namespace noRestForTheQuery
         const int WINDOW_HEIGHT = 600;
         const float GRAVITY = 0.95F;
 
+        //Game tools
+        public static int defaultBlockSize = 50;
+        public static int gameLevel;
+        public static Random rand = new Random();
+
         //Student
         Texture2D studentSprite;
-        Student student;
+        Student1 student;
         float studentSpeed = 3.5F;
 
         //Testing platform array
@@ -33,7 +35,6 @@ namespace noRestForTheQuery
 
         //Misc
         KeyboardState oldKeyState = Keyboard.GetState();
-        Random rand = new Random();
 
         public Game1()
         {
@@ -54,12 +55,18 @@ namespace noRestForTheQuery
             platformSprite = Content.Load<Texture2D>(@"Sprites/simplePlatform");
 
             //Student starts in the top center of the screen for testing
-            student = new Student(studentSprite, new Vector2(WINDOW_WIDTH / 2 - studentSprite.Width / 2, 200 ));
+            student = new Student1( new Vector2(WINDOW_WIDTH / 2 - studentSprite.Width / 2, 200 ), 
+                                    new Vector2(studentSprite.Width/2, studentSprite.Height/2),
+                                    Vector2.Zero );
 
             //One platform in the middle of the screen to test
-            platforms.Add(new Platform(new Vector2(100, 400), 600, 20));
-            platforms.Add(new Platform(new Vector2(500, 300), 100, 20));
-            platforms.Add(new Platform(new Vector2(200, 350), 100, 50));
+            for (int i = 100; i < 700; i += defaultBlockSize)
+            {
+                platforms.Add(new Platform(new Vector2(i, 400), Vector2.Zero, 2));
+            }
+            platforms.Add(new Platform(new Vector2(100, 250), Vector2.Zero, 1));
+            platforms.Add(new Platform(new Vector2(250, 250), Vector2.Zero, 1));
+            platforms.Add(new Platform(new Vector2(400, 350), Vector2.Zero, 1));
         }
         protected override void UnloadContent()
         {
@@ -80,7 +87,7 @@ namespace noRestForTheQuery
                     student.velocity.Y = 0; //Guarantees a full jump
                     student.velocity.Y -= 15;
                     student.jumping = true;
-                    student.collided = false;
+                    student.onGround = false;
                 }
 
                 //Lateral movement
@@ -91,14 +98,12 @@ namespace noRestForTheQuery
 
             student.position.X += student.velocity.X;
             student.position.Y += student.velocity.Y;
-            student.collisionRectangle.X = (int)student.position.X;
-            student.collisionRectangle.Y = (int)student.position.Y;
 
 
             //Bookkeeping
             student.velocity.Y += GRAVITY;                              //Influence of gravity
             handleStudentPlatformCollision();                           //Handle student/platform collision
-            if (student.velocity.Y == 0 && student.jumping ) { student.jumping = false; }   //Reset jump state
+            if (student.velocity.Y == 0 && student.onGround) { student.jumping = false; }   //Reset jump state
             if (oldKeyState.IsKeyUp(Keys.A) || oldKeyState.IsKeyUp(Keys.D)) { student.velocity.X = 0; }
             oldKeyState = Keyboard.GetState();
             base.Update(gameTime);
@@ -111,10 +116,10 @@ namespace noRestForTheQuery
             for (int i = 0; i < platforms.Count; ++i)
             {
                 spriteBatch.Draw(platformSprite, platforms[i].position,
-                                 platforms[i].collisionRectangle, Color.Black);
+                                 platforms[i].rectangle, Color.Black);
             }
 
-            spriteBatch.Draw(student.sprite, student.position, Color.White);
+            spriteBatch.Draw(studentSprite, student.position, Color.White);
 
             spriteBatch.End();
             base.Draw(gameTime);
@@ -124,50 +129,59 @@ namespace noRestForTheQuery
             int interLeft, interRight, interTop, interBot, interWidth, interHeight;
             for (int i = 0; i < platforms.Count; ++i)
             {
-                interLeft = Math.Max(student.collisionRectangle.Left, platforms[i].collisionRectangle.Left);
-                interTop = Math.Max(student.collisionRectangle.Top, platforms[i].collisionRectangle.Top);
-                interRight = Math.Min(student.collisionRectangle.Right, platforms[i].collisionRectangle.Right);
-                interBot = Math.Min(student.collisionRectangle.Bottom, platforms[i].collisionRectangle.Bottom);
+                interLeft = Math.Max((int)student.position.X, platforms[i].rectangle.Left);
+                interTop = Math.Max((int)student.position.Y, platforms[i].rectangle.Top);
+                interRight = Math.Min((int)student.position.X + studentSprite.Width, platforms[i].rectangle.Right);
+                interBot = Math.Min((int)student.position.Y + studentSprite.Height, platforms[i].rectangle.Bottom);
                 interWidth = interRight - interLeft;
                 interHeight = interBot - interTop;
 
                 if (interWidth >= 0 && interHeight >= 0) //If the intersecting rect is valid, they hit!
                 {
-                    student.collided = true;
- 
-                    //Left and Right collision
-                    if(interHeight > interWidth)
+                    student.colliding = true;
+
+                    //Movement collision on ground
+                    if (interHeight > interWidth)
                     {
-                        //If they were walking to the right, stop them at the edge
+                        //If student going to the right, stop them at the left edge of the platform
                         if (student.velocity.X > 0) { student.position.X -= interWidth; }
 
-                        //If they were walking to the left, stop them at the edge
-                        else if( student.velocity.X < 0 ) { student.position.X += interWidth; }
+                        //If going to the left
+                        if (student.velocity.X < 0) { student.position.X += interWidth; }
 
+                        //They collided, so stop moving
                         student.velocity.X = 0;
                     }
 
-                    //Top collision
-                    else if (interWidth > 0)
+                    //Vertical movement collision
+                    if (interWidth > interHeight)
                     {
-                        //If they were falling, stop them and move them to the obstacle edge
-                        if (student.velocity.Y > 0)
+                        //If student is falling, stop them at the top edge of the platform (Make sure it's above the platform)
+                        if (student.velocity.Y > 0 && student.position.Y < platforms[i].position.Y)
                         {
-                            student.position.Y -= interHeight;
+                            student.position.Y -= interHeight; 
+                            student.onGround = true;
                             student.velocity.Y = 0;
+                        }
+
+                        //If student is going upward from a jump, put them down (w/ a little force)
+                        if (student.velocity.Y < 0) 
+                        { 
+                            student.position.Y += interHeight; 
+                            student.velocity.Y *= -.1F; 
                         }
                     }
                 }
+                else { student.colliding = false; }
             }
         }
-
         protected void reset()
         {
             student.position.X = WINDOW_WIDTH / 2 - studentSprite.Width / 2;
             student.position.Y = 200;
             student.velocity = Vector2.Zero;
             student.jumping = false;
-            student.collided = false;
+            student.colliding = false;
         }
     }
 }
